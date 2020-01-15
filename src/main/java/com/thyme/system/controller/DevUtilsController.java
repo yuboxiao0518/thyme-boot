79f7ec719860f3f7e4293811a5ae220dc15830dd
thyme <1608025239@qq.com>
2020-01-15 20:51:52 +0800

    菜单图标选择页面添加

---
+++ src/main/java/com/thyme/system/controller/DevUtilsController.java
@@ -0,0 +1,21 @@
+package com.thyme.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author thyme
+ * @ClassName DevUtilsController
+ * @Description TODO
+ * @Date 2020/1/15 16:05
+ */
+@Controller
+@RequestMapping("/devUtils")
+public class DevUtilsController {
+
+    @GetMapping("/menuIcon")
+    public String menuIcon() {
+        return "/module/devutils/menuIcon";
+    }
+}
