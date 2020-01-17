fbf865ae3ff91514025e9dea5729f9e056b5f5ab
C_G_520 <1336443640@qq.comom>
2020-01-17 11:13:01 +0800

    2020-01-17 基本资料添加手机号和邮箱的验证

--- src/main/resources/static/js/modules/user/personal.js
+++ src/main/resources/static/js/modules/user/personal.js
@@ -23,20 +23,22 @@ var app =  new Vue({
         submitForm:function(formName) {
             this.$refs[formName].validate((valid) => {
                 if (valid) {
-                    $.ajax({
-                        url: context + 'user/editUser',
-                        type: 'POST',
-                        data: JSON.stringify(app.ruleForm),
-                        dataType : 'json',
-                        contentType:'application/json',
-                        success: function (res) {
-                            if (res.code === 200) {
-                                app.$message.success(res.message);
-                            } else {
-                                app.$message.error(res.message);
+                    if (isMobileEmail(app.ruleForm.mobile,app.ruleForm.email)){
+                        $.ajax({
+                            url: context + 'user/editUser',
+                            type: 'POST',
+                            data: JSON.stringify(app.ruleForm),
+                            dataType : 'json',
+                            contentType:'application/json',
+                            success: function (res) {
+                                if (res.code === 200) {
+                                    app.$message.success(res.message);
+                                } else {
+                                    app.$message.error(res.message);
+                                }
                             }
-                        }
-                    })
+                        })
+                    }
                 } else {
                     console.log('error submit!!');
                     return false;
@@ -58,3 +60,26 @@ var app =  new Vue({
         this.ruleForm.liveAddress = sysUser.liveAddress;
     }
 });
+
+function isMobileEmail(mobile,email) {
+    var flag = true;
+    if (mobile !== ""){
+        var phoneReg = /^[1][3,4,5,7,8][0-9]{9}$/;
+        if (!phoneReg.test(mobile)) {
+            app.$message.error("手机号格式不正确，请重新输入！");
+            app.ruleForm.mobile = "";
+            flag =  false;
+        }
+    }
+    if(email !== "") {
+        var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+        //调用正则验证test()函数
+        isok= reg.test(email);
+        if(!isok) {
+            app.$message.error("邮箱格式不正确，请重新输入！");
+            app.ruleForm.email = "";
+            flag = false;
+        }
+    }
+    return flag;
+}
